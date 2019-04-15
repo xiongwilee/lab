6a3909642c78e49340b108f84c3a4b3692020924
xiongwilee <xiongwilee@foxmail.com>
2019-04-15 20:53:29 +0800

    add: 添加开方方法

--- squareRoot.js
+++ squareRoot.js
@@ -1,9 +1,11 @@
 /**
  * 整数开方
- * @param {*} i 需要开方的数字
+ * @param {Number} i 需要开方的数字
  * @todo
  *  1. 如果是i为浮点型，可以先转为整型再调用该方法
  *  2. 递归体中可以做优化，不一定从0开始迭代
+ * 
+ * @return {Number|Boolean}
  */
 module.exports = function squareRoot(i){
   // 首先判断是否为数字
